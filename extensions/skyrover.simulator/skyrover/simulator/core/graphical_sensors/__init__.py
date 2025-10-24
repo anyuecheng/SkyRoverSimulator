05aa67258a7a179c41fd41d2e604d178a3690cb6
wangfei <651277897@qq.com>
2025-10-24 20:22:52 +0800

    add graphical sensor classes.

---
+++ extensions/skyrover.simulator/skyrover/simulator/core/graphical_sensors/__init__.py
@@ -0,0 +1,8 @@
+"""
+| Author: Fei Wang (feiwang@dlmu.edu.cn)
+| License: BSD-3-Clause. Copyright (c) 2025, Fei Wang. All rights reserved.
+"""
+
+from .graphical_sensor import GraphicalSensor
+from .monocular_camera import MonocularCamera
+from .lidar import Lidar
